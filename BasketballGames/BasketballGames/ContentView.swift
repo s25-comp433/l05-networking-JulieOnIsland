988b360cb352669e06d3b58b1f320ac0ea51278a
Juhyun Lee <juhyun62015@gmail.com>
2025-02-28 17:28:31 -0500

    basketball

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -1,23 +1,79 @@
-//
-//  ContentView.swift
-//  BasketballGames
-//
-//  Created by Samuel Shi on 2/27/25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
 }
+
+struct Game: Codable, Identifiable {
+    var team: String
+    var id: Int
+    var isHomeGame: Bool
+    var score: Score
+    var date: String
+    var opponent: String
+}
+
+struct ContentView: View {
+    @State private var games = [Game]()
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            Text("UNC Basketball")
+                .font(.largeTitle)
+                .bold()
+                .padding(.leading)
+            
+            List(games) { game in
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("\(game.team) vs. \(game.opponent)")
+                            .font(.title3)
+                        Spacer()
+                        Text("\(game.score.unc) - \(game.score.opponent)")
+                            .font(.title3)
+                    }
+                    HStack {
+                        Text("\(game.date)")
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                        Spacer()
+                        Text(game.isHomeGame ? "Home" : "Away")
+                            .foregroundColor(.gray)
+                            .font(.subheadline)
+                    }
+                }
+                .padding(.vertical, 5)
+            }
+        }
+        .task {
+            await loadData()
+        }
+  
+ 
+           }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decoder = JSONDecoder()
+            games = try decoder.decode([Game].self, from: data)
+        } catch {
+            print("Invalid data: \(error.localizedDescription)")
+        }
+    }
+  
+}
+    
+
+ 
+
 
 #Preview {
     ContentView()
--- iTunesSearch/iTunesSearch/ContentView.swift
+++ iTunesSearch/iTunesSearch/ContentView.swift
@@ -44,8 +44,13 @@ struct ContentView: View {
         
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
         } catch {
             print("invalid data")
+            
         }
     }
 }
